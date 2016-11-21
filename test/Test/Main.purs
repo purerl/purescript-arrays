e3a1f30d4936b7a667203ef4b6923c5452e1c9d0
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-11-21 20:54:19 +0000

    Erl Remove ST

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -8,10 +8,8 @@ import Control.Monad.Eff.Console (CONSOLE)
 import Test.Assert (ASSERT)
 import Test.Data.Array (testArray)
 import Test.Data.Array.Partial (testArrayPartial)
-import Test.Data.Array.ST (testArrayST)
 
 main :: forall eff. Eff (console :: CONSOLE, assert :: ASSERT | eff) Unit
 main = do
   testArray
-  testArrayST
   testArrayPartial
